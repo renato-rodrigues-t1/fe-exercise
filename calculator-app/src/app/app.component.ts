3869e97c27239d4322ed5ee66a088515ec63e4d2
renato.rodrigues-t1 <renato.rsufu@gmail.com>
2025-05-15 16:05:59 +0100

    Calc component

--- calculator-app/src/app/app.component.ts
+++ calculator-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculatorComponent } from "./components/calculator/calculator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ calculator-app/src/app/components/calculator/calculator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent {
+
+}
